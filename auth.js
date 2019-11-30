592b1fbf2eef98e0f1f7c775cff64bfca7fd063d
axiotidis <50782505+axiotidis@users.noreply.github.com>
2019-11-30 02:47:20 +0200

    Update auth.js

--- auth.js
+++ auth.js
@@ -12,7 +12,7 @@ const uiConfig = {
     },
   },
   signInFlow: 'popup',
-  signInSuccessUrl: 'done.html',
+  signInSuccessUrl: 'app.html',
   signInOptions: [
     firebase.auth.EmailAuthProvider.PROVIDER_ID,
     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
