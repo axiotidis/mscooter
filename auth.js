40d0e722dfc8948f7cbb0e9cc3bcd1eed7228ca9
axiotidis <50782505+axiotidis@users.noreply.github.com>
2019-12-01 02:05:22 +0200

    Update auth.js

--- auth.js
+++ auth.js
@@ -16,7 +16,7 @@ const uiConfig = {
   signInOptions: [
     firebase.auth.EmailAuthProvider.PROVIDER_ID,
     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    firebase.auth.GithubAuthProvider.PROVIDER_ID,
+    //firebase.auth.GithubAuthProvider.PROVIDER_ID,
     // Additional login options should be listed here
     // once they are enabled within the console.
   ],
