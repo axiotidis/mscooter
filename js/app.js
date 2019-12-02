4d5f77eff6a43bbe239ba1a0bf2727821a1907fd
axiotidis <50782505+axiotidis@users.noreply.github.com>
2019-12-02 21:03:12 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ var basemap = new L.TileLayer(baseUrl, {maxZoom: 19, attribution: baseAttributio
 
 var lat = 0;		//set initial value latitude
 var lng = 0;		//set initial value lognitude
-var zoom = 17;		//set zoom level
+var zoom = 15;		//set zoom level
 var myPosition = 0;	//set an initial value of user's location
 
 var sPos = [];		//this array holds the position of e-scooters
@@ -27,7 +27,8 @@ var redIcon = L.icon({			//set a personal marker icon
 	//shadowSize:   [50, 64], // size of the shadow
 	iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
 	//shadowAnchor: [4, 62],  // the same for the shadow
-	popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+	//popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+	popupAnchor:  [-6, -76] // point from which the popup should open relative to the iconAnchor
 });
 
 //center and zoom map in a position found by geolocation
