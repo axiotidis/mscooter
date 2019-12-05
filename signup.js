89ac4176c55caf4ff00b0099c6564f4bea476f36
axiotidis <50782505+axiotidis@users.noreply.github.com>
2019-12-06 00:06:45 +0200

    Update signup.js

--- signup.js
+++ signup.js
@@ -50,7 +50,7 @@ function submitForm(e){
 
     setTimeout(function(){
         document.querySelector('.continue').style.display = 'none';
-        window.location.replace("map-master/escooter_map.html");
+        window.location.replace("map-master/signin.html");
     },4000);
 
 }
