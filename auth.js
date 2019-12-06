48b6c9377396283f879c1ca06cb6d525bb75b5d4
axiotidis <50782505+axiotidis@users.noreply.github.com>
2019-12-07 00:42:35 +0200

    Update auth.js

--- auth.js
+++ auth.js
@@ -15,7 +15,7 @@ const uiConfig = {
   signInSuccessUrl: 'app.html',
   signInOptions: [
     firebase.auth.EmailAuthProvider.PROVIDER_ID,
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    //firebase.auth.GoogleAuthProvider.PROVIDER_ID,
     //firebase.auth.GithubAuthProvider.PROVIDER_ID,
     // Additional login options should be listed here
     // once they are enabled within the console.
