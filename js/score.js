294fee602fef0712440791c245744bb2aa62f1a8
axiotidis <50782505+axiotidis@users.noreply.github.com>
2019-12-08 00:15:08 +0200

    Update score.js

--- js/score.js
+++ js/score.js
@@ -91,17 +91,22 @@ var ctx = document.getElementById('myChart').getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: [userNickname, 'Χρίστος', 'Αλεξάνδρα', 'Μιχάλης', 'Περικλής', 'Γιώργος'],
+        labels: [stringOfNicknames],
         datasets: [{
             label: 'This week\'s scoring board',
-            data: [userPoints, 100, 70, 50, 200, 30],
+            data: [stringOfPoints],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
                 'rgba(255, 206, 86, 0.2)',
                 'rgba(75, 192, 192, 0.2)',
                 'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(255, 159, 64, 0.2)',
+				'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
             ],
             borderColor: [
                 'rgba(255, 99, 132, 1)',
@@ -109,7 +114,12 @@ var myChart = new Chart(ctx, {
                 'rgba(255, 206, 86, 1)',
                 'rgba(75, 192, 192, 1)',
                 'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgba(255, 159, 64, 1)',
+				'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
             ],
             borderWidth: 1
         }]
