4905f7dc4df4a9096c1a5d4e76ba59615e3e2ae0
axiotidis <50782505+axiotidis@users.noreply.github.com>
2019-12-08 00:34:32 +0200

    Update score.js

--- js/score.js
+++ js/score.js
@@ -70,43 +70,26 @@ function gotData(data){
 	var stringOfNicknames = "";
 	var stringOfPoints = "";
 	
-	for (var i = 0; i < numberOfUsers; ++i){
-		if (usersArray[i].nickname == userNickname){
-			stringOfNicknames = stringOfNicknames.concat(usersArray[i].nickname + " (You) " + ", ");
-			idUser = i;
-		}else {
-		stringOfNicknames = stringOfNicknames.concat(usersArray[i].nickname + ", ");
-		}
-		stringOfPoints = stringOfPoints.concat(usersArray[i].points + ", ");
-		//console.log("User " + currentNickname + " have " + currentPoints + " points");
-		
-	}
-	console.log(stringOfNicknames);
-	console.log(stringOfPoints);
-	console.log("User ID = " + idUser);
-});
+	
+	
+
 	
 
 var ctx = document.getElementById('myChart').getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: [stringOfNicknames],
+        labels: [],
         datasets: [{
             label: 'This week\'s scoring board',
-            data: [stringOfPoints],
+            data: [],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
                 'rgba(255, 206, 86, 0.2)',
                 'rgba(75, 192, 192, 0.2)',
                 'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)',
-				'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
             ],
             borderColor: [
                 'rgba(255, 99, 132, 1)',
@@ -114,12 +97,7 @@ var myChart = new Chart(ctx, {
                 'rgba(255, 206, 86, 1)',
                 'rgba(75, 192, 192, 1)',
                 'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)',
-				'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
             ],
             borderWidth: 1
         }]
@@ -134,7 +112,18 @@ var myChart = new Chart(ctx, {
         }
     }
 });
-
+for (var i = 0; i < numberOfUsers; ++i){
+		if (usersArray[i].nickname == userNickname){
+			myChart.data.labels.push(usersArray[i].nickname + " (You)");
+			idUser = i;
+		}else {
+		myChart.data.labels.push(usersArray[i].nickname);
+		}
+		myChart.data.datasets.data.push(usersArray[i].points);
+		//console.log("User " + currentNickname + " have " + currentPoints + " points");
+		
+	}
+});
 }
 
 function errData(error){
